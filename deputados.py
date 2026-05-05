6083afbadf519adededb8e1146a8699cc234be60
sofiacandido-collab <sofiacandidoseixas@gmail.com>
2026-05-05 15:07:52 -0300

    Update deputados.py

--- deputados.py
+++ deputados.py
@@ -4,13 +4,11 @@ import pandas as pd
 st.write("**Esse é um comparador dos deputados de 2018 e 2022**")
 
 st.write("Deputados de 2018")
-df2 = pd.read_csv('deputados_2018 (1).csv')
-st.dataframe(df2)
-
 st.write("Deputados de 2022")
-df = pd.read_csv('deputados_2022.csv')
-st.dataframe(df)
+df_2018 = pd.read_csv('deputados_2018 (1).csv')
+df_2022 = pd.read_csv('deputados_2022.csv')
 
+df_merge = pd.merge(df_2018, df_2022, on='nome', how='outer')
 st.title('Buscador de deputadas por partido: ')
 partido = st.text_input('Digite a sigla do partido: ')
 
@@ -19,3 +17,4 @@ if partido:
   filtrado2 = df[df['sexo'].str.upper() == sexo.upper()]
   st.dataframe(filtrado + filtrado2)
   
+
