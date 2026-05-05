bbc12af1fe48b40d61a558bb4d09f1253a0a6c65
sofiacandido-collab <sofiacandidoseixas@gmail.com>
2026-05-05 14:47:34 -0300

    Update deputados.py

--- deputados.py
+++ deputados.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import pandas as pd
 
-st.write("*Esse é um comparador dos deputados de 2018 e 2022*")
+st.write("**Esse é um comparador dos deputados de 2018 e 2022**")
 df = pd.read_csv('deputados_2022.csv')
 st.dataframe(df)
+
+df
 
