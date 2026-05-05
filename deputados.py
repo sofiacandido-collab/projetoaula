6ca97288370901b3eb03e19eb046dd2609501d8b
sofiacandido-collab <sofiacandidoseixas@gmail.com>
2026-05-05 14:53:51 -0300

    Update deputados.py

--- deputados.py
+++ deputados.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 st.write("**Esse é um comparador dos deputados de 2018 e 2022**")
 
-st.write(""Deputados de 2018"")
+st.write("Deputados de 2018")
 df2 = pd.read_csv('deputados_2018 (1).csv')
 st.dataframe(df2)
 
